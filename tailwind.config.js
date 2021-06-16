a4df316fd1081e8542c0a318890328d10b75ae1e
JohnRay1997 <johnkomarnickicontact@gmail.com>
2021-06-16 08:45:06 -0400

    Updated Starting Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,7 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      Poppins: ["Poppins, sans-serif"],
-    },
-    extend: {
-      colors: {
-        "bookmark-purple": "#5267DF",
-        "bookmark-red": "#FA5959",
-        "bookmark-dark-blue": "#242A45",
-        "bookmark-light-grey": "#9194A2",
-        "bookmark-lynx-white": "#f7f7f7",
-      },
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-      screens: {
-        lg: "1124px",
-        xl: "1124px",
-        "2xl": "1124px",
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
